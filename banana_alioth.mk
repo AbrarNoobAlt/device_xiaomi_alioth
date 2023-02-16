b447e9a8360b2f5cb5f27feccfd9fc95a1f46a5c
AbrarNoob <hasinabrarhaque@gmail.com>
2023-02-16 23:26:28 +0600

    alioth: Init BananaDroid

--- banana_alioth.mk
+++ banana_alioth.mk
@@ -8,13 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common BananaDroid stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
+
+#Banana Stuffs
+BANANA_BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
+BANANA_MAINTAINER := Abrar
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := banana_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
